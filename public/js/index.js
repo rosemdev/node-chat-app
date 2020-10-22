8767d4636ca990edf0ae7f398196f7440ceb5566
rosem <romasemenyshyn@gmail.com>
2020-10-23 00:01:59 +0300

    Added new room/ existing forms' interaction

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,13 @@
 const socket = io();
 console.log(socket);
 
+//Elements
 const roomsEl = document.querySelector('#rooms');
+const select = roomsEl.querySelector('select');
+const newRoomBtn = document.querySelector('.new-room');
+const newRoomForm = document.querySelector('.new-room-block');
+const selectRoomBtn = document.querySelector('.select-room');
+const joinBtn = document.querySelector('.join-block');
 
 //Templates
 const roomsTemplate = document.querySelector('#rooms-template').innerHTML;
@@ -9,20 +15,32 @@ const roomsTemplate = document.querySelector('#rooms-template').innerHTML;
 
 
 socket.on('connect', () => {
-    console.log(socket.connected); 
+    console.log(socket.connected);
+
+    if(!select) { 
+        selectRoomBtn.style.display = 'none'
+    } else {
+        selectRoomBtn.style.display = 'block'
+    }
 });
 
 socket.on('getRoomsList', ({roomsList}) => {
-
-    console.log(roomsList);
-    
-
     const html = Mustache.render(roomsTemplate, {
         roomsList
     });
-
-    roomsEl.innerHTML = html;
+    
+    roomsEl.innerHTML = html;    
 });
 
+newRoomBtn.addEventListener('click', (event) => {  
+    newRoomForm.style.display = 'block';
+    joinBtn.style.display = 'block';
+});
 
-console.log('index.js');
+selectRoomBtn.addEventListener('click', (event) => {  
+    
+    let select = roomsEl.querySelector('select');
+    roomsEl.style.display = 'block';
+    joinBtn.style.display = 'block';
+   
+});
