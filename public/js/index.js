d8c25cabe24b23214c30d9126f4c0a33c5ec956c
rosem <romasemenyshyn@gmail.com>
2020-10-25 15:39:53 +0200

    Changed firt page froms' structure, added selected room funcitonality

--- public/js/index.js
+++ public/js/index.js
@@ -2,45 +2,96 @@ const socket = io();
 console.log(socket);
 
 //Elements
-const roomsEl = document.querySelector('#rooms');
-const select = roomsEl.querySelector('select');
+const roomChoiceBlock = document.querySelector('.room-choice-block');
+
+//new room
 const newRoomBtn = document.querySelector('.new-room');
-const newRoomForm = document.querySelector('.new-room-block');
+const newRoomForm = document.querySelector('.new-room-form');
+
+//select room
 const selectRoomBtn = document.querySelector('.select-room');
-const joinBtn = document.querySelector('.join-block');
+const selectRoomForm = document.querySelector('.existing-room-form');
+const roomsListBlock = document.querySelector('#rooms');
 
 //Templates
 const roomsTemplate = document.querySelector('#rooms-template').innerHTML;
 
+//Veriables
+let availableRooms = [];
 
 
 socket.on('connect', () => {
     console.log(socket.connected);
+    showHideRoomChoice();
 
-    if(!select) { 
-        selectRoomBtn.style.display = 'none'
-    } else {
-        selectRoomBtn.style.display = 'block'
-    }
 });
 
 socket.on('getRoomsList', ({roomsList}) => {
     const html = Mustache.render(roomsTemplate, {
         roomsList
     });
+
+    availableRooms = roomsList;
+    roomsListBlock.innerHTML = html; 
+
+    showHideRoomChoice();
+
+    let roomSelect = document.querySelector('.rooms');
     
-    roomsEl.innerHTML = html;    
+        roomSelect.addEventListener('change', function() {
+            console.log('You selected: ', this.value);
+
+            socket.emit('selectedRoom', {selectedRoom: this.value}, () => {
+                console.log('selectedRoom is shared');  
+            });
+        });
+
 });
 
-newRoomBtn.addEventListener('click', (event) => {  
-    newRoomForm.style.display = 'block';
-    joinBtn.style.display = 'block';
+//Event listeners
+
+newRoomBtn.addEventListener('click', () => {
+
+    //if selectRoomForm is visible then turn it off
+    if(selectRoomForm.classList.contains('selected')) {
+        selectRoomForm.classList.remove('selected');
+        selectRoomForm.style.display = 'none';
+    }
+
+    newRoomForm.classList.add('selected');
+
+
+    if(newRoomForm.classList.contains('selected')) {
+        newRoomForm.style.display = 'block';
+    }
+    
 });
 
-selectRoomBtn.addEventListener('click', (event) => {  
+selectRoomBtn.addEventListener('click', () => {
     
-    let select = roomsEl.querySelector('select');
-    roomsEl.style.display = 'block';
-    joinBtn.style.display = 'block';
-   
+    //if newRoomForm is visible then turn it off
+    if(newRoomForm.classList.contains('selected')) {
+        newRoomForm.classList.remove('selected');
+        newRoomForm.style.display = 'none';
+    }
+    
+    selectRoomForm.classList.add('selected');
+
+    if(selectRoomForm.classList.contains('selected')) {
+        selectRoomForm.style.display = 'block';
+    }    
+
 });
+
+
+//helper functions
+
+function showHideRoomChoice() {
+    if(availableRooms.length === 0) {
+        roomChoiceBlock.style.display = 'none';
+        newRoomForm.style.display = 'block';
+    } else {
+        roomChoiceBlock.style.display = 'block';
+        newRoomForm.style.display = 'none';
+    }  
+}
